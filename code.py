61169790585309dc6f64631017c52a0a66731e9b
Ibrahim <ibipriya@gmail.com>
2019-04-26 23:22:11 +0530

    Updated the else statement to check for similar words from data "json file" and print the closest matching word

--- code.py
+++ code.py
@@ -1,10 +1,16 @@
 import json
 from difflib import get_close_matches
-data = json.load(open('data (1).json'))
+data = json.load(open('data .json'))
 word= input('Enter the word : \n')
 def translation(w):
     return data[w]
 if word in data:
-    return translation(word)
+    print(translation(word))
 else:
-    return f'Word does not exist'
+    closest=get_close_matches(word,data,n=1,cutoff=0.8)
+
+    choice=input(f' Did you mean {closest[0]} ? Y or N \n')
+    if choice== 'Y':
+        print(translation(closest[0]))
+    else:
+        print('word does not exist')
